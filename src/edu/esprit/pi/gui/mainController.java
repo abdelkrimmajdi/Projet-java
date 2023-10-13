dcc70d9bba9de210c1778bd6a2bda221b8a428aa
abdelkrimmajdi <majdi.abdelkrim@esprit.com>
2023-10-13 19:42:20 +0100

    projet

---
+++ src/edu/esprit/pi/gui/mainController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.esprit.pi.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author aziz
+ */
+public class mainController extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
