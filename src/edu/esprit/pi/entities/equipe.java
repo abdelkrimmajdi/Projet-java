401bd32d92e9aa16ccfdd46cb56642f6ea091cc7
oudaybn <ouday.benmessaoud@esprit.tn>
2023-10-23 16:40:46 +0100

    entiteeq

--- src/edu/esprit/pi/entities/equipe.java
+++ src/edu/esprit/pi/entities/equipe.java
@@ -1,64 +1,75 @@
 package edu.esprit.pi.entities;
 
 
-public class equipe {
-
-    public static int getNbjoueur() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+public class equipe {                   
+    private int id_equipe;
+    private String nom_equipe;
+    private int nbjoueur_equipe;
+    private String couleurmaillot;
+    
+    
+    public equipe() {
     }
-    private int id;
-    private String nom;
-    private int nbjoueur;
     
-    
-    public equipe(){
+    public equipe(String nom_equipe, int nbjoueur_equipe, String couleurmaillot) {
+        this.nom_equipe = nom_equipe;
+        this.nbjoueur_equipe = nbjoueur_equipe;
+        this.couleurmaillot=couleurmaillot;
+    }
+
+    public equipe(int id_equipe, String nom_equipe, int nbjoueur_equipe ,String couleurmaillot ) {
+        this.id_equipe = id_equipe;
+        this.nom_equipe = nom_equipe;
+        this.nbjoueur_equipe = nbjoueur_equipe;
+                this.couleurmaillot=couleurmaillot;
+
+    }
+
+    public String getCouleurmaillot() {
+        return couleurmaillot;
+    }
+
+    public void setCouleurmaillot(String couleurmaillot) {
+        this.couleurmaillot = couleurmaillot;
+    }
+
+    public int getId_equipe() {
+        return id_equipe;
+    }
+
+    public void setId_equipe(int id_equipe) {
+        this.id_equipe = id_equipe;
+    }
+
+    public String getNom_equipe() {
+        return nom_equipe;
+    }
+
+    public void setNom_equipe(String nom_equipe) {
+        this.nom_equipe = nom_equipe;
+    }
+
+    public int getNbjoueur_equipe() {
+        return nbjoueur_equipe;
+    }
+
+    public void setNbjoueur_equipe(int nbjoueur_equipe) {
+        this.nbjoueur_equipe = nbjoueur_equipe;
         
-    }
-
-    public equipe(String nom, int nbjoueur) {
-        this.nom = nom;
-        this.nbjoueur = nbjoueur;
-    }
-
-    public equipe(int id, String nom, int nbjoueur) {
-        this.id = id;
-        this.nom = nom;
-        this.nbjoueur = nbjoueur;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public int getnbjoueur() {
-        return nbjoueur;
-    }
-
-    public void setnbjoueur(int nbjoueur) {
-        this.nbjoueur = nbjoueur;
     }
 
     @Override
     public String toString() {
-        return "equipe{" +"=id" + id + "nom=" + nom + ", prenom=" + nbjoueur + '}';
+        return "equipe{" + "id_equipe=" + id_equipe + ", nom_equipe=" + nom_equipe + ", nbjoueur_equipe=" + nbjoueur_equipe + ", couleurmaillot=" + couleurmaillot + '}';
     }
+
+ 
+   
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 17 * hash + this.id;
+        int hash = 3;
+        hash = 23 * hash + this.id_equipe;
         return hash;
     }
 
@@ -74,10 +85,13 @@ public class equipe {
             return false;
         }
         final equipe other = (equipe) obj;
-        if (this.id != other.id) {
+        if (this.id_equipe != other.id_equipe) {
             return false;
         }
         return true;
     }
+    
+   
+  
     
 }
